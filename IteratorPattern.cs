71bdd2c6cf2f6e95587ecc6900a8c5387c8edd92
Tobi Denagan <denagantobi@gmail.com>
2022-09-09 09:56:01 +0100

    Add project files.

---
+++ IteratorPattern.cs
@@ -0,0 +1,66 @@
+﻿    using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDesignPatterns
+{
+    internal class IteratorPattern
+    {
+    }
+
+    class MonthWithDays
+    {
+        public int Days { get; set; }
+        public string Date { get; set; }
+    }
+
+    class DaysInMonthEnumerator : IEnumerator<MonthWithDays>
+    {
+        private int year = 1;
+        private int month = 0;
+        public MonthWithDays Current => new MonthWithDays() 
+        {
+            Date = $"{year.ToString().PadLeft(4, '0')}-{month}",
+            Days = DateTime.DaysInMonth(year, month)
+        };
+
+        object IEnumerator.Current => Current;
+
+        public void Dispose()
+        {
+        }
+
+        public bool MoveNext()
+        {
+            month++;
+            if (month > 12)
+            {
+                month = 1;
+                year++;
+            }
+            return year < 5;
+        }
+
+        public void Reset()
+        {
+            month = 0;
+            year = 1;
+        }
+    }
+
+    class DaysInMonthCollection : IEnumerable<MonthWithDays>
+    {
+        public IEnumerator<MonthWithDays> GetEnumerator()
+        {
+            return  new DaysInMonthEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using CSharpDesignPatterns;
+
+var collection = new DaysInMonthCollection();
+foreach (var monthWithDays in collection)
+{
+    Console.WriteLine($"{monthWithDays.Date} - {monthWithDays.Days}");
+}
