8d432ecb19e169ab0f4a1199767a6c28cbae1a7e
Tobi Denagan <denagantobi@gmail.com>
2022-09-10 10:07:15 +0100

    Iterator 2 added

---
+++ IteratorPattern2.cs
@@ -0,0 +1,21 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDesignPatterns
+{
+    public static class EnumerableExtensions
+    {
+        // call method on an enumerable, then specify the filter (where) and then
+        // the transformation (select) you want
+        public static IEnumerable<TResult> FilteredSelect<T, TResult>(
+            this IEnumerable<T> enumerable, Func<T, bool> filter,
+            Func<T, TResult> fieldSelector)
+        {
+            foreach (var a in enumerable)
+                if (filter(a)) yield return fieldSelector(a);    
+        }
+    }
+}
